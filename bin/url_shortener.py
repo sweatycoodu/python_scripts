69f1b849cf7b0545cf3235a9b310713debc1f2df
Sam Cole <113649989+sweatycoodu@users.noreply.github.com>
2022-10-04 12:43:55 +1100

    code linting for url_shortener.py

--- bin/url_shortener.py
+++ bin/url_shortener.py
@@ -1,14 +1,15 @@
-# A simple script to shorten URLs from the shell, using the TinyURL API.
+"""
+A simple script to shorten URLs from the shell, using the TinyURL API.
+"""
 
 # Import the required modules for the script to function.
 
 import urllib.request
 import urllib.parse
 
-# Function to declare the URL to be shortened, prompted to the user.
-
 
 def get_url():
+    """Function to declare the URL to be shortened, prompted to the user."""
     url = input("Source URL: ")
     # Display an error message if the URL is invalid.
     if not url.startswith('http://') and not url.startswith('https://'):
@@ -16,38 +17,35 @@ def get_url():
         return get_url()
     return url
 
-# Function to shorten the URL, using the TinyURL API.
-
 
 def shorten_url(url):
+    """Function to shorten the URL, using the TinyURL API."""
     request_url = ('https://tinyurl.com/api-create.php?' +
                    urllib.parse.urlencode({'url': url}))
     with urllib.request.urlopen(request_url) as response:
         return response.read().decode('utf-8')
 
-# Function to print the shortened URL to the user.
-
 
 def print_short_url(short_url):
-    print("Shortened URL: {}".format(short_url))
-
-# Function to repeat the process, if the user wishes to shorten another URL.
+    """Function to print the shortened URL to the user."""
+    print(f"Shortened URL: {short_url}")
 
 
 def repeat():
+    """Function to repeat the process, if the user wishes to shorten another URL."""
     while True:
         again = input("Shorten another URL? (y/n): ")
         if again.lower() == 'y':
             return True
-        elif again.lower() == 'n':
+        if again.lower() == 'n':
             return False
-        else:
-            print("Invalid input. Please try again.")
 
-# Main function to run the script.
+
+print("Invalid input. Please try again.")
 
 
 def shorten_go():
+    """Main function to run the script."""
     while True:
         url = get_url()
         short_url = shorten_url(url)
